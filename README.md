524929d411c2630b64a801be39a0e2313f7be818
Iñigo Alonso Ruiz <inigol22zgz@gmail.com>
2017-03-18 00:01:54 +0100

    test and train folder

--- README.md
+++ README.md
@@ -5,8 +5,8 @@ The main aim of this project is to learn tensorflow framework. I will start almo
 First of all, I will set some steps/aims (related to the stuff I want to learn). As I complete the goals, I will be reporting the outcomes as well as the git commits which contains the new code.
 
 
-- [ ] Create an script to automatize the image collection process. For example given 10 lables to classify, get images from google searches and save the data-set ready to train
-- [ ] Choose what to Classify
+- [x] Create an script to automatize the image collection process. For example given 10 lables to classify, get images from google searches and save the data-set ready to train
+- [x] Choose what to Classify
 - [ ] Load the data. Whether with the [tensorflow API](https://www.tensorflow.org/programmers_guide/reading_data) or just create a function which loads a random image/batch from a random labels, inside the main training loop, see [this example](https://github.com/asabater94/exeinos-uCode-2017/tree/master/ML), or numpy, or [DIY](http://stackoverflow.com/questions/34340489/tensorflow-read-images-with-labels) loading the filepath and label and just [reading](http://stackoverflow.com/questions/39195113/how-to-load-multiple-images-in-a-numpy-array ) the batch when needed with a nextBatch funtion
 - [ ] Save and load the model in order to finetune and not to loose the training.
 - [ ] Make a test script which gives both the predicted label and the probability.
@@ -21,9 +21,23 @@ First of all, I will set some steps/aims (related to the stuff I want to learn).
 - [ ] Make a distributed version
 
 ## Image colletor. Scrapping the web. Automatizing the process
-The labels to classify have to be specified on a file.
+The collecting data system is being automatized. The data is collected via google searches and is goint to be saved in a test & train directory as well as in one folder per each label. Each line ofthe [lables files](https://github.com/Shathe/Learning-Tensorflow/blob/master/labels.txt) is going to be a label to classify. In each line are more than one possible query, each one separated by comas.
 
+In order to download all the images this command has to be executed:
+```
+python getData.py --dataFolder data # data is the folder to download the images
+```
+Then you can resize all the images just like this:
+```
+python resizeImages.py --dataFolder data --width 360 --heigh 320
+```
 ## Choose what to Classify
-First of all, I will begin to classify places like: beach, forest, city sight, city street, village sight, indoors (building). When I get this point, I am setting up clearl the labels.
+First of all, I will begin to classify [places](https://github.com/Shathe/Learning-Tensorflow/blob/master/labels.txt):
+* beach
+* forest
+* mountain
+* city street
+* village street
+* building inside
 
-Sendondly, I am thinking to classify something more useful such as some car models (for instance 10 models of Audi)
+Once I can classify those landscapse, I am thinking to classify something more useful such as some car models (for instance 10 models of Audi).
--- getData.py
+++ getData.py
@@ -3,39 +3,56 @@ import urllib2
 import os
 import json
 import argparse
+import random
 
 
 def get_soup(url, header):
     return BeautifulSoup(urllib2.urlopen(urllib2.Request(url, headers=header)), 'html.parser')
-'''
+
+
 parser = argparse.ArgumentParser()
 parser.add_argument("--dataFolder", help="folder where the images are going to be saved")
 args = parser.parse_args()
 DATA_FILE_NAME = args.dataFolder
-'''
-DATA_FILE_NAME = "data"
+TRAIN_PATH = DATA_FILE_NAME + "/train"
+TEST_PATH = DATA_FILE_NAME + "/test"
+percentage_train = 0.8
+
 # Read each label
 with open("labels.txt") as f:
     content = f.readlines()
 # you may also want to remove whitespace characters like `\n` at the end of each line
 labels = [x.strip() for x in content]
 
-# Creates the directory where the data is going to be saved
+# Creates the directories where the data is going to be saved
 if not os.path.exists(DATA_FILE_NAME):
     os.makedirs(DATA_FILE_NAME)
 
-print labels
+if not os.path.exists(TRAIN_PATH):
+    os.makedirs(TRAIN_PATH)
+
+if not os.path.exists(TEST_PATH):
+    os.makedirs(TEST_PATH)
 
 for label in labels:
     # Creates a directory for each label
+    print label
     all_names = label.split(',')
-    name = all_names[0]
+    name = all_names[0]  # Gests the name of the label
 
-    LABEL_PATH = os.path.join(DATA_FILE_NAME, name)
-    if not os.path.exists(LABEL_PATH):
-        os.makedirs(LABEL_PATH)
+    LABEL_PATH_TRAIN = os.path.join(TRAIN_PATH, name)
+    LABEL_PATH_TEST = os.path.join(TEST_PATH, name)
+    if not os.path.exists(LABEL_PATH_TRAIN):
+        os.makedirs(LABEL_PATH_TRAIN)
+    if not os.path.exists(LABEL_PATH_TEST):
+        os.makedirs(LABEL_PATH_TEST)
 
+    cntr = 0
+    err = 0
+    # For each label, several queries
     for name_i in all_names:
+
+        # Prepare query
         query = name_i.split()
         query = '+'.join(query)
         url = "https://www.google.co.in/search?q=" + query + "&source=lnms&tbm=isch"
@@ -43,33 +60,51 @@ for label in labels:
         # add the directory for your image here
         header = {
             'User-Agent': "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"
-            }
+        }
+        # Query
         soup = get_soup(url, header)
 
+        # Get the data
         ActualImages = []  # contains the link for Large original images, type of  image
         for a in soup.find_all("div", {"class": "rg_meta"}):
             link, Type = json.loads(a.text)["ou"], json.loads(a.text)["ity"]
             ActualImages.append((link, Type))
 
-        print  "there are total", len(ActualImages), "images"
+        print "there are total", len(ActualImages), "images"
 
+        # Save each image
         for i, (img, Type) in enumerate(ActualImages):
             try:
                 req = urllib2.Request(img, headers={'User-Agent': header})
                 raw_img = urllib2.urlopen(req).read()
+                # percentage_train out of 1 will be save in the training folder
+                PATH_TO_SAVE = LABEL_PATH_TRAIN
+                if random.uniform(0, 1) > percentage_train:
+                    PATH_TO_SAVE = LABEL_PATH_TEST
 
-                cntr = len([i for i in os.listdir(LABEL_PATH) if name_i in i]) + 1
                 if cntr % 20 == 0:
                     print cntr
+
+                download = False
+                # Only save jpg or png or jpeg
                 if len(Type) == 0:
                     if "jpeg" in raw_img and "body" not in raw_img:
-                        f = open(os.path.join(LABEL_PATH, name_i + "_" + str(cntr) + ".jpeg" + Type), 'wb')
+                        f = open(os.path.join(PATH_TO_SAVE, name_i + "_" + str(cntr) + ".jpeg" + Type), 'wb')
+                        download = True
                     elif "png" in raw_img and "body" not in raw_img:
-                        f = open(os.path.join(LABEL_PATH, name_i + "_" + str(cntr) + ".png" + Type), 'wb')
-                elif "body" not in raw_img and ("jpg" in Type or "png" in Type):
-                    f = open(os.path.join(LABEL_PATH, name_i + "_" + str(cntr) + "." + Type), 'wb')
-
-                f.write(raw_img)
-                f.close()
+                        f = open(os.path.join(PATH_TO_SAVE, name_i + "_" + str(cntr) + ".png" + Type), 'wb')
+                        download = True
+                    elif "jpg" in raw_img and "body" not in raw_img:
+                        f = open(os.path.join(PATH_TO_SAVE, name_i + "_" + str(cntr) + ".jpg" + Type), 'wb')
+                        download = True
+                elif "body" not in raw_img and ("jpg" in Type or "png" in Type or "jpeg" in Type):
+                    f = open(os.path.join(PATH_TO_SAVE, name_i + "_" + str(cntr) + "." + Type), 'wb')
+                    download = True
+                if download:
+                    cntr += 1
+                    f.write(raw_img)
+                    f.close()
             except Exception as e:
-                print "could not load : " + img
+                err += 1
+
+    print ("for label" + name + " images well downloaded: " + cntr + ", images not downloaded: " + err)
